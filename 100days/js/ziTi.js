214f93e42779b62b8db6752350751885e65994f2
tmachc <hanchong@witmob.com>
2014-07-25 19:41:09 +0800

    Revert "Revert "添加js""
    This reverts commit 33876f73991fac5908b9e963edd25ce4e9e643da.

---
+++ 100days/js/ziTi.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 14-7-25.
+ */
+
+$(document).ready(function(){
+    $(".ziTiBut").click(function(){
+        $("#ceshiziti a").attr("style","font-family:" + this.name);
+    })
+});
